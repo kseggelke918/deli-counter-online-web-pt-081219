55e8ec2e31b65b3731ee224e0959cebbf1c3d6cb
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-18 21:27:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,23 +1,31 @@
 
-def line(array)
-  count = 1 
-  new_array = []
-  if array == []
-    puts "The line is currently empty."
-  else 
-    array.each do |customer|
-    new_array << count
-    new_array << ". "
-    new_array << customer
-    if new_array.length < ((array.length)*4)-1 
-      new_array << " "
-    end 
-    count += 1 
-    end   
-    deli_line = new_array.join 
+# def line(array)
+#   count = 1 
+#   new_array = []
+#   if array == []
+#     puts "The line is currently empty."
+#   else 
+#     array.each do |customer|
+#     new_array << count
+#     new_array << ". "
+#     new_array << customer
+#     if new_array.length < ((array.length)*4)-1 
+#       new_array << " "
+#     end 
+#     count += 1 
+#     end   
+#     deli_line = new_array.join 
       
-     puts "The line is currently: #{deli_line}"
+#     puts "The line is currently: #{deli_line}"
+#   end 
+# end
+
+def line(array)
+  new_array = []
+  array.each_with_index do |customer, index|
+    new_array << "#{index + 1}. #{customer}"
   end 
+  puts "The line is currently: #{new_array.join(" ")}"
 end
 
 def take_a_number(current_line, person_joining)
