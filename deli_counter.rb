e88e6c7f96a10959d8100a4c63bc6a64e5d55968
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-18 21:16:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -25,8 +25,6 @@ def take_a_number(current_line, person_joining)
     current_line << person_joining
     puts "Welcome, #{person_joining}. You are number 1 in line."
   else 
-    line(current_line)
-    current_line << person_joining
-    puts "Welcome #{person_joining}. You are number #{count} in line." 
+    line(current_line) 
   end 
 end 
