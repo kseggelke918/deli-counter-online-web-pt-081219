0fc88b6bb7624797fe61859ef65946a84f5cc15f
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-18 20:53:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,7 +9,7 @@ def line(array)
     new_array << count
     new_array << ". "
     new_array << customer
-    if new_array.length <= (array.length)*4) 
+    if new_array.length <= (array.length)*4 
       new_array << " "
     end 
     count += 1 
