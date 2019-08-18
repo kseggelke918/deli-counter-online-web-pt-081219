8a93520597be07ea38cf939ffda91d4f652b638f
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-18 20:19:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+def line(array)
+  count = 1 
+  new_array = []
+  if array == []
+    puts "The line is currently empty."
+  else 
+    array.each do |customer|
+    new_array << count
+    new_array << customer
+    count += 1 
+    end 
+    return "The line is currently: #{new_array}"
+  end 
+end 
