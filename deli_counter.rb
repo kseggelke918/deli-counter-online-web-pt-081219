4e8f720ffa922edf647fc482ce4be58e3e36cd6d
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-18 21:35:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,25 +1,3 @@
-
-# def line(array)
-#   count = 1 
-#   new_array = []
-#   if array == []
-#     puts "The line is currently empty."
-#   else 
-#     array.each do |customer|
-#     new_array << count
-#     new_array << ". "
-#     new_array << customer
-#     if new_array.length < ((array.length)*4)-1 
-#       new_array << " "
-#     end 
-#     count += 1 
-#     end   
-#     deli_line = new_array.join 
-      
-#     puts "The line is currently: #{deli_line}"
-#   end 
-# end
-
 def line(array)
   new_array = []
   if array == []
