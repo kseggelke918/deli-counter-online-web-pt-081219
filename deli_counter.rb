c7a0f64399388087813ce17219af5923547dfb55
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-18 20:31:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,11 +5,10 @@ def line(array)
     puts "The line is currently empty."
   else 
     array.each do |customer|
-    new_array << " "
     new_array << count
     new_array << ". "
     new_array << customer
-    #new_array << " "
+    new_array << " "
     count += 1 
     end 
     line = new_array.join 
